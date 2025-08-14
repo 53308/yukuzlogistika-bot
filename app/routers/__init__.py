5d803986332cd3ca6469f09b0c1cbe334eff535b
53308 <5330897@gmail.com>
2025-08-15 04:20:00 +0500

    __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,6 +2,7 @@
 Routers package for the Yukuz Logistics Bot
 """
 
-from . import admin, cargo, search, start, transport, language, detail_view
+from . import admin, search, detail_view
+from . import start, cargo, transport, language
 
-__all__ = ["admin", "cargo", "search", "start", "transport", "language", "detail_view"]
+__all__ = ["admin", "search", "detail_view", "start", "cargo", "transport", "language"]
