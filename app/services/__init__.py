2b8d33a644404120f24dedde6e9c0adb9848bca3
53308 <5330897@gmail.com>
2025-08-15 17:19:48 +0500

    __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,7 @@
-# Services package
+"""
+Routers package for the Yukuz Logistics Bot
+"""
+
+from . import admin, cargo, search, start, transport, language
+
+__all__ = ["admin", "cargo", "search", "start", "transport", "language"]
